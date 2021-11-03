46cfd8a8a119a0de01ddec7960a9d13f84f7359d
Leandro <psychomusicbox@gmail.com>
2021-11-03 11:54:29 -0500

    Bug responsive de selector de tipo arreglado

--- js copy/effects.js
+++ js copy/effects.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 var btnTypes = document.getElementById('btnTypes');
 var types = document.querySelector('.types');
 var desc = document.querySelector('.calculadora__desc');
@@ -11,17 +10,4 @@ btnTypes.addEventListener('click', function(e) {
     typeTitle.classList.toggle('active');
     queryBtnTypes.classList.toggle('active');
 });
-=======
-var btnTypes = document.getElementById('btnTypes');
-var types = document.querySelector('.types');
-var desc = document.querySelector('.calculadora__desc');
-var typeTitle = document.querySelector('.type__title');
-var queryBtnTypes = document.querySelector('#btnTypes');
 
-btnTypes.addEventListener('click', function(e) {
-    types.classList.toggle('active');
-    // desc.classList.toggle('active');
-    typeTitle.classList.toggle('active');
-    queryBtnTypes.classList.toggle('active');
-});
->>>>>>> 2c7988b481bdd19f7b14615f196dae05e558f5c9
