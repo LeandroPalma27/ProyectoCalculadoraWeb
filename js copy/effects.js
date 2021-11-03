2c7988b481bdd19f7b14615f196dae05e558f5c9
Leandro <87090321+LeandroPalma27@users.noreply.github.com>
2021-11-03 11:51:30 -0500

    Add files via upload

--- js copy/effects.js
+++ js copy/effects.js
@@ -1,10 +1,12 @@
 var btnTypes = document.getElementById('btnTypes');
 var types = document.querySelector('.types');
 var desc = document.querySelector('.calculadora__desc');
-var typeTitle = document.querySelector('.type__title')
+var typeTitle = document.querySelector('.type__title');
+var queryBtnTypes = document.querySelector('#btnTypes');
 
 btnTypes.addEventListener('click', function(e) {
     types.classList.toggle('active');
-    desc.classList.toggle('active');
+    // desc.classList.toggle('active');
     typeTitle.classList.toggle('active');
+    queryBtnTypes.classList.toggle('active');
 });
